2cc9c5c5186e7204699de56482f6401aafb1bb0e
abib05001 <abib05001@E-PW0234M2>
2023-09-15 13:07:41 +0200

    Add project files.

---
+++ uppgift 2,2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Abdullahi
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur högt hoppade Elin i meter");
+            string LångText = Console.ReadLine();
+            Console.WriteLine("Hur högt hoppade Alma i meter?");
+            string HoppadeText = Console.ReadLine();
+            double långt = double.Parse(LångText);
+            double hoppade = double.Parse(HoppadeText);
+            double skillnad = långt - hoppade; 
+                
+            Console.WriteLine("Elin hoppade " + skillnad + " meter mer än Alma");
+        }
+    }
+}
